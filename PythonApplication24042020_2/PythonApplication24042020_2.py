78552fc02fa12b950e06168195cee992bc7f5d5f
DESKTOP-LHRQ2R4\adamsives <adamsives@gmail.com>
2020-05-01 16:48:42 +1000

    First commit - it works - can ship in debug mode

--- PythonApplication24042020_2/PythonApplication24042020_2.py
+++ PythonApplication24042020_2/PythonApplication24042020_2.py
@@ -1,10 +1,56 @@
-from math import radians
-import numpy as np     # installed with matplotlib
-import matplotlib.pyplot as plt
+import csv
+import requests
+import json
+import jellyfish
+import easygui
+import sys
 
-def main():
-    x = np.arange(0, radians(1800), radians(12))
-    plt.plot(x, np.cos(x), 'b')
-    plt.show()
+baseUrl = 'https://api.datamuse.com/words?'
+startsWith = 'sp='
+soundsLike = 'sl='
 
-main()
+#1. create a list of words to get potential rhymes with as testName[]
+
+testNames = []
+potentialMetaphones = []
+#promt user for input test names
+filename = easygui.fileopenbox()
+
+if not filename:
+    sys.exit()
+
+#eventual output in the same directory
+outputFileType = (filename[filename.rfind("."):filename.rfind(".")+5])
+outputFile = filename[0: filename.rfind(".")] + "_metaphones" + outputFileType
+
+f = open(outputFile, "w")
+
+with open(filename) as csvDataFile:
+    csvReader = csv.reader(csvDataFile)
+    for row in csvReader:
+        testNames.append(row[0])
+
+#call endpoint with this test name
+    for name in testNames:
+        matchFlag = False
+        startingLetterParam = startsWith + name[0:2].lower() + '*&'
+        soundsLikeLetterParam = soundsLike + name.lower()
+        url = baseUrl + startingLetterParam + soundsLikeLetterParam
+       
+        # GET potential rhymes
+        response = requests.get(url)
+        homophoneList = response.json() #list
+    
+        if homophoneList:
+            for homophone in homophoneList:
+                if homophone["score"] > 89 and jellyfish.metaphone(name) == jellyfish.metaphone(homophone["word"]) and name.upper() != homophone["word"].upper():
+                    matchFlag = True
+                    #print(name + "," + homophone["word"].title())#---------------------------------------debug
+                    f.write(name + "," + homophone["word"].title())
+                    f.write("\n")
+        else:#--------------------------------------------------------------------------------------------debug
+            print(name + ":" + "no homophone returned")#--------------------------------------------------debug
+            matchFlag = True#--------------------------------------------------debug
+
+        if matchFlag != True:
+            print(name + ":" + "no good match")
